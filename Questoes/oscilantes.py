02892f62f5e96abfff0bc5eb1fe86b9382b47343
iasmimtx <93227509+iasmimtx@users.noreply.github.com>
2022-09-28 21:10:21 -0300

    Update oscilantes.py

--- Questoes/oscilantes.py
+++ Questoes/oscilantes.py
@@ -9,9 +9,3 @@ for i in range(len(codigo)-1):
             verificacao += 'verdadeiro'
             break
 print(f'{verificacao}: {len(codigo)} algarismos.')
-
-        
-'''Escreva um programa que receba um código gerado para acesso a um formulário online e verifica se ele é "verdadeiro" ou "falso".
- Um código verificado como "verdadeiro" é composto por uma sequência de dígitos que se alternam entre pares e ímpares ou entre ímpares e pares.
-Não são admitidos dígitos pares ou ímpares juntos.
- Por exemplo, o código 7852 é verificado como verdadeiro, porque 7 é ímpar; 8 é par; 5 é ímpar e 2 é par.'''

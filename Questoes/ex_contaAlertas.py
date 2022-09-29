b81bb2dcfa9c168feed0da2a874d3d30c5ecf7dd
iasmimtx <93227509+iasmimtx@users.noreply.github.com>
2022-09-28 21:07:47 -0300

    Update ex_contaAlertas.py

--- Questoes/ex_contaAlertas.py
+++ Questoes/ex_contaAlertas.py
@@ -10,6 +10,3 @@ medicoes = [50, 50, 50, 23, 21, 17, 15, 60, 65, 15, 15]
 #assert conta_alertas_acude(medicoes) == 2
 
 print(conta_alertas_acude([50, 50, 50, 23, 21, 17, 15, 60, 65, 15, 15]))
-'''O critério para descartar medições não confiáveis é que a diferença (em módulo) de uma medição e a
-anterior seja maior ou igual a 10. Assim, por exemplo, se a n-ésima medição for 15 e a (n+1)-ésima for 60,
-pode-se assumir que a medição é não confiável e que, portanto, o alarme não deveria ter sido emitido.'''
